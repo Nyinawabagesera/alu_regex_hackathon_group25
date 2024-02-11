990509b4cd45c58a51d166cc8c7dc60a769477b1
Nyinawabagesera <j.nyinawaba@alustudent.com>
2024-02-11 20:44:31 +0000

    file

--- regex-app.py
+++ regex-app.py
@@ -1,6 +1,6 @@
 import re
 
-text = "Fish (2002), 01-10-2000, ISBN 123-4-123-12345-1, 192.098.190.075"
+text = "Fish (2002), 01-10-2000, ISBN 123-4-123-12345-1, 192.098.190.075, #ffffff, Fish S01E02:Little Rat"
 
 
 #Regular Expression
@@ -8,15 +8,21 @@ movie_title_regex = r'.*\s\(\d{4}\)'
 date_regex = r'\d{2}-[A-Za-z]{3}-\d{4}'
 isbn_regex = r'ISBN \d{3}-\d-\d{3}-\d{5}-\d'
 ip_address_regex = r'((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4}'
+hex_color_regex = r'#([A-Fa-f0-9]{6})'
+tv_episode_regex = r'.* S\d{2}E\d{2}: .*'
 
 #Extracting Data
 movie_titles = re.findall(movie_title_regex, text)
 dates = re.findall(date_regex, text)
 isbn_numbers = re.findall(isbn_regex, text)
 ip_addresses = re.findall(ip_address_regex, text)
+hex_colors = re.findall(hex_color_regex, text)
+tv_episode_titles = re.findall(tv_episode_regex, text)
 
 #Printing Extracted Data
 print("Movie Titles:", movie_titles)
 print("Dates:", dates)
 print("ISBN Numbers:", isbn_numbers)
 print("IP Addresses:", ip_addresses)
+print("Hex Color Codes:", hex_colors)
+print("TV Episode Titles:", tv_episode_titles)
