c5c4f3b46fcce75d8fb39d2d97da2b8f26c2649b
ndivine-eng <144015345+ndivine-eng@users.noreply.github.com>
2024-02-11 22:50:18 +0200

    new commit

--- regex-app.py
+++ regex-app.py
@@ -1,6 +1,6 @@
 import re
 
-text = "Fish (2002), 01-10-2000, ISBN 123-4-123-12345-1, 192.098.190.075, #ffffff, Fish S01E02:Little Rat"
+text = "Fish (2002), 01-10-2000, ISBN 123-4-123-12345-1, 192.098.190.075, #ffffff, Fish S01E02:Little Rat, @Ghyghi, [Verse 1] I love you..., Why did the car run ? Because nothing"
 
 
 #Regular Expression
@@ -10,6 +10,9 @@ isbn_regex = r'ISBN \d{3}-\d-\d{3}-\d{5}-\d'
 ip_address_regex = r'((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4}'
 hex_color_regex = r'#([A-Fa-f0-9]{6})'
 tv_episode_regex = r'.* S\d{2}E\d{2}: .*'
+song_lyrics_regex = r'\[(\w+ \d+)\]\s(.*?)\.\.\.'
+twitter_username_regex = r'@\w+'
+jokes_regex = r'Why did the .*? Because.*\,'
 
 #Extracting Data
 movie_titles = re.findall(movie_title_regex, text)
@@ -18,6 +21,9 @@ isbn_numbers = re.findall(isbn_regex, text)
 ip_addresses = re.findall(ip_address_regex, text)
 hex_colors = re.findall(hex_color_regex, text)
 tv_episode_titles = re.findall(tv_episode_regex, text)
+song_lyrics = re.findall(song_lyrics_regex, text)
+twitter_usernames = re.findall(twitter_username_regex, text)
+jokes = re.findall(jokes_regex, text)
 
 #Printing Extracted Data
 print("Movie Titles:", movie_titles)
@@ -26,3 +32,6 @@ print("ISBN Numbers:", isbn_numbers)
 print("IP Addresses:", ip_addresses)
 print("Hex Color Codes:", hex_colors)
 print("TV Episode Titles:", tv_episode_titles)
+print("Song Lyrics:", lyrics_array)
+print("Twitter Usernames:", twitter_usernames)
+print("Jokes:", jokes)
